0ccff487a8b5ac44f9e8abd1bf5afe7abbadc2c4
koubaki <koutavaki1000@gmail.com>
2025-06-16 16:59:29 +0300

    Added tests

--- test/index.js
+++ test/index.js
@@ -1,5 +1,23 @@
-// TODO: Implement tests
-console.info('TODO')
+import { readFileSync } from 'fs'
 
-// Exit
-process.exit(1)
+import { createApp } from '../dist/index.js'
+
+// Ask question
+console.log('Assuming you have built the project, would you like to continue? (y/n)')
+
+// Observe answer
+if (readFileSync(0, 'utf-8').trim() === 'y') {
+  // Create the app
+  const app = createApp('test')
+
+  // Set up static files
+  app.staticServer()
+
+  // Start the server
+  app.start(3000, () => {
+    console.log('Test server is running on http://localhost:3000')
+  })
+} else {
+  // Exit the process
+  process.exit(1)
+}
