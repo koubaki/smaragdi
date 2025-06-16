bbbb5c792f76b39be87d7897dbee78553d8b7bcf
koubaki <koutavaki1000@gmail.com>
2025-06-16 16:18:53 +0300

    Created more middleware

--- src/createApp.ts
+++ src/createApp.ts
@@ -1,5 +1,15 @@
+import path from 'path'
+
 import express, { Application as ExpressApplication, json } from 'express'
 import cookieParser from 'cookie-parser'
+
+// Extend Request interface for middleware
+declare module 'express-serve-static-core' {
+  interface Request {
+    unNormalizedUrl?: string
+    unNormalizedOriginalUrl?: string
+  }
+}
 
 import Application from './Application'
 import Environment from './Environtment'
@@ -15,6 +25,27 @@ const createApp = (env?: Environment): Application => {
   // Environment setting
   app.set('env', env ?? process.env?.NODE_ENV ?? Environment.Development)
 
+  // Normalizate of URIs
+  app.use((req, res, next) => {
+    // Store unnormalized URLs
+    req.unNormalizedUrl = req.url
+    req.unNormalizedOriginalUrl = req.originalUrl
+
+    // Normalize URL
+    req.url = path.normalize(req.url).replace(/\\/g, '/').replace(/\/$/, '')
+    if (req.url === '') req.url = '/'
+
+    // Normalize original URL
+    req.originalUrl = path.normalize(req.originalUrl).replace(/\\/g, '/').replace(/\/$/, '')
+    if (req.originalUrl === '') req.originalUrl = '/'
+
+    next()
+  })
+
+
+  // Default JSON middleware
+  app.jsonMiddleware = json()
+
   // Configuration for JSON middleware
   app.json = (options?: object): void => {
     app.jsonMiddleware = json(options)
@@ -26,7 +57,7 @@ const createApp = (env?: Environment): Application => {
   // Cookie parser middleware
   app.use(cookieParser())
 
-  // Promotion of Smaragdi
+  // Promote Smaragdi
   app.use((req, res, next) => {
     res.setHeader('X-Powered-By', 'Smaragdi')
     next()
